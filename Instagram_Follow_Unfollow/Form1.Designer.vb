edd32b3a06c9f12710d72293b209e253c785aa76
rjwilder1 <69400635+rjwilder1@users.noreply.github.com>
2021-05-28 00:39:17 -0500

    Add files via upload

---
+++ Instagram_Follow_Unfollow/Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(73, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(268, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "8228worldwide"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Location = New System.Drawing.Point(73, 41)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(247, 20)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "Cark2001$"
+        Me.TextBox2.UseSystemPasswordChar = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(4, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(4, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.Location = New System.Drawing.Point(5, 171)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox3.Size = New System.Drawing.Size(336, 204)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(4, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "User To Follow"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox4.Location = New System.Drawing.Point(88, 67)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(253, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(4, 93)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Photo Link"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.TextBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox5.Location = New System.Drawing.Point(73, 93)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.ReadOnly = True
+        Me.TextBox5.Size = New System.Drawing.Size(268, 20)
+        Me.TextBox5.TabIndex = 10
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CheckBox1.Location = New System.Drawing.Point(103, 119)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(100, 17)
+        Me.CheckBox1.TabIndex = 12
+        Me.CheckBox1.Text = "Follow Followers"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CheckBox2.Location = New System.Drawing.Point(221, 119)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(112, 17)
+        Me.CheckBox2.TabIndex = 13
+        Me.CheckBox2.Text = "Follow Photo Likes"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(5, 142)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(328, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CheckBox3.Location = New System.Drawing.Point(12, 119)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(64, 17)
+        Me.CheckBox3.TabIndex = 14
+        Me.CheckBox3.Text = "Unfollow"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(324, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(17, 23)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "S"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(4, 378)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 13)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Status:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(45, 378)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(45, 13)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Inactive"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(345, 395)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Instagram Bot"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Instagram_Follow_Unfollow/Form1.vb
@@ -0,0 +1,342 @@
+﻿Imports System.Threading
+Imports OpenQA.Selenium
+Imports OpenQA.Selenium.Chrome
+Public Class Form1
+    Dim Count As Integer = 0
+    Dim Viewing As Boolean = True
+    Public Function NL(txt As String)
+        TextBox3.Text += "[" + TimeOfDay.ToString("h:mm:ss tt") + "] " + txt & vbNewLine
+        Return 0
+    End Function
+    Function Login(Yo As String)
+        Label6.Text = "Running"
+        CheckBox1.Enabled = False
+        CheckBox2.Enabled = False
+        CheckBox3.Enabled = False
+        Dim UserName = "#loginForm > div > div:nth-child(1) > div > label > input"
+        Dim Password = "#loginForm > div > div:nth-child(2) > div > label > input"
+        Dim LogButton = "#loginForm > div > div:nth-child(3) > button > div"
+        Dim SuccessLogin = "body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.HoLwm"
+        Button1.Enabled = False
+        Dim options = New ChromeOptions()
+        options.AddArguments("--no-sandbox", "--disable-web-security", "--disable-gpu", "--incognito", "--proxy-bypass-list=*", "--proxy-server='direct://'", "--log-level=3", "--hide-scrollbars", "--disable-extensions") 'Path To your chrome profile   "user-data-dir=C:\Users\RJ\AppData\Local\Google\Chrome\User Data", 
+        Dim drivserv = ChromeDriverService.CreateDefaultService("C:\")
+        drivserv.HideCommandPromptWindow = True
+        Dim Browser = New ChromeDriver(drivserv, options)
+        AddHandler MyBase.FormClosing, Sub(sender As Object, e As FormClosingEventArgs)
+                                           Browser.Quit()
+                                           Application.Exit()
+                                           Me.Close()
+                                       End Sub
+        Browser.Manage().Window().Position = New Point(100000, 10000)
+        Browser.Navigate.GoToUrl("https://instagram.com")
+        Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10)
+        'Logging in
+        NL("Logging into " + TextBox1.Text)
+        Browser.FindElementByCssSelector(UserName).SendKeys(TextBox1.Text)
+        Browser.FindElementByCssSelector(Password).SendKeys(TextBox2.Text)
+        Browser.FindElementByCssSelector(LogButton).Click()
+        Try
+            Browser.FindElementByCssSelector(SuccessLogin)
+            NL("Successfully logged in.")
+
+        Catch ex As Exception
+            NL("Failed to log in. Please restart the program and try again.")
+            Return 1
+        End Try
+        If Yo = "Unfollow" Then
+            Dim Thrd = New Thread(Sub() Unfollow(Browser))
+            Thrd.Start()
+        ElseIf Yo = "Follow" Then
+            Dim Thrd = New Thread(Sub() FollowFollowers(Browser))
+            Thrd.Start()
+        ElseIf Yo = "FollowLikes" Then
+            Dim Thrd = New Thread(Sub() FollowLikes(Browser))
+            Thrd.Start()
+        End If
+        'Button1.Enabled = False
+        Return 0
+    End Function
+
+    Function Unfollow(Browser As OpenQA.Selenium.Chrome.ChromeDriver)
+        AddHandler MyBase.FormClosing, Sub(sender As Object, e As FormClosingEventArgs)
+                                           Browser.Quit()
+                                           Application.Exit()
+                                           Me.Close()
+                                       End Sub
+        Button1.Enabled = False
+        If Count = 3 Then
+            NL("Finished unfollowing")
+            Count = 0
+            Return 0
+        End If
+        Browser.Navigate.GoToUrl("https://instagram.com/" + TextBox1.Text)
+        Dim Following = "#react-root > section > main > div > header > section > ul > li:nth-child(3) > a"
+        Dim UnfollowConfirm = "body > div:nth-child(19) > div > div > div > div.mt3GC > button.aOOlW.-Cab_"
+        Dim TotalFollowing = "#react-root > section > main > div > header > section > ul > li:nth-child(3) > a > span"
+        Dim UnfollowNum As Integer = Int(Browser.FindElementByCssSelector(TotalFollowing).Text)
+        NL("Amount to unfollow: " + Browser.FindElementByCssSelector(TotalFollowing).Text)
+        Browser.FindElementByCssSelector(Following).Click()
+        Thread.Sleep(1000)
+        NL("Starting to unfollow " + Browser.FindElementByCssSelector(TotalFollowing).Text + " users")
+        For p As Integer = 1 To UnfollowNum
+            Try
+                Dim rd As New Random()
+                Dim RanNum As Integer = rd.Next(1000, 5000)
+                Thread.Sleep(RanNum)
+                Try
+                    Dim par = Browser.FindElement(By.XPath("/html/body/div[5]/div/div/div[2]/ul/div/li[" + p.ToString + "]/div/div[3]"))
+                    Dim lis = par.FindElements(By.XPath(".//*"))
+                    For Each i In lis
+                        Browser.ExecuteScript("arguments[0].scrollIntoView();", i)
+                        i.Click()
+                        Try
+                            Browser.FindElementByCssSelector(UnfollowConfirm).Click()
+                        Catch ex As Exception
+                        End Try
+                        Thread.Sleep(2500)
+                        If i.Text = "Following" Then
+                            NL("Cannot follow anymore... Sleeping for 5 minutes.")
+                            For p1 As Integer = 0 To 300000
+                                Thread.Sleep(1000)
+                                Label6.Text = "Sleeping for " + p1.ToString() + "ms"
+                            Next
+
+                        End If
+                    Next
+                Catch ex As Exception
+                    Dim par = Browser.FindElement(By.XPath("/html/body/div[5]/div/div/div[2]/ul/div/li[" + p.ToString + "]/div/div[2]"))
+                    Dim lis = par.FindElements(By.XPath(".//*"))
+                    For Each i In lis
+                        Browser.ExecuteScript("arguments[0].scrollIntoView();", i)
+                        i.Click()
+                        Try
+                            Browser.FindElementByCssSelector(UnfollowConfirm).Click()
+                        Catch ex1 As Exception
+                        End Try
+                        Thread.Sleep(2500)
+                        If i.Text = "Following" Then
+                            NL("Cannot follow anymore... Sleeping for 5 minutes.")
+                            For p1 As Integer = 0 To 300000
+                                Thread.Sleep(1000)
+                                Label6.Text = "Sleeping for " + p1.ToString() + "ms"
+                            Next
+                        End If
+                    Next
+                End Try
+                NL("Unfollowed " + p.ToString + "/" + UnfollowNum.ToString())
+            Catch ex As Exception
+                NL("An error occured... Sleeping for 5 minutes.")
+                For p1 As Integer = 0 To 300000
+                    Thread.Sleep(1000)
+                    Label6.Text = "Sleeping for " + p1.ToString() + "ms"
+                Next
+            End Try
+        Next
+        Count += 1
+        NL("Rerunning to make sure all are unfollowed. " + Count.ToString() + "/3")
+        Unfollow(Browser)
+        Button1.Enabled = True
+        Browser.Quit()
+        CheckBox1.Enabled = True
+        CheckBox2.Enabled = True
+        CheckBox3.Enabled = True
+        Label6.Text = "Inactive"
+        Return 0
+    End Function
+    Function FollowFollowers(Browser As OpenQA.Selenium.Chrome.ChromeDriver)
+        AddHandler MyBase.FormClosing, Sub(sender As Object, e As FormClosingEventArgs)
+                                           Browser.Quit()
+                                           Application.Exit()
+                                           Me.Close()
+                                       End Sub
+        Button1.Enabled = False
+        Browser.Navigate.GoToUrl("https://instagram.com/" + TextBox4.Text)
+        Dim Following = "#react-root > section > main > div > header > section > ul > li:nth-child(2) > a"
+        Dim TotalFollowing = "#react-root > section > main > div > header > section > ul > li:nth-child(2) > a > span"
+        Dim UnfollowNum As Integer = Int(Browser.FindElementByCssSelector(TotalFollowing).GetAttribute("title"))
+        NL("Amount to follow: " + Browser.FindElementByCssSelector(TotalFollowing).GetAttribute("title"))
+        Browser.FindElementByCssSelector(Following).Click()
+        Thread.Sleep(1000)
+        NL("Starting to follow " + Browser.FindElementByCssSelector(TotalFollowing).GetAttribute("title") + " users")
+        For p As Integer = 1 To UnfollowNum
+            Try
+                Dim rd As New Random()
+                Dim RanNum As Integer = rd.Next(1000, 5000)
+                Thread.Sleep(RanNum)
+                Try
+                    Dim par = Browser.FindElement(By.XPath("/html/body/div[5]/div/div/div[2]/ul/div/li[" + p.ToString + "]/div/div[3]"))
+                    Dim lis = par.FindElements(By.XPath(".//*"))
+                    For Each i In lis
+                        Browser.ExecuteScript("arguments[0].scrollIntoView();", i)
+                        i.Click()
+
+                    Next
+                Catch ex As Exception
+                    Dim par = Browser.FindElement(By.XPath("/html/body/div[5]/div/div/div[2]/ul/div/li[" + p.ToString + "]/div/div[2]"))
+                    Dim lis = par.FindElements(By.XPath(".//*"))
+                    For Each i In lis
+                        Browser.ExecuteScript("arguments[0].scrollIntoView();", i)
+                        i.Click()
+                    Next
+                End Try
+                NL("Followed " + p.ToString + "/" + UnfollowNum.ToString())
+            Catch ex As Exception
+                NL("Cannot follow anymore... Sleeping for 5 minutes.")
+                For p1 As Integer = 0 To 300000
+                    Thread.Sleep(1000)
+                    Label6.Text = "Sleeping for " + p1.ToString() + "ms"
+                Next
+            End Try
+        Next
+        NL("Finished following " + UnfollowNum.ToString())
+        Button1.Enabled = True
+        Browser.Quit()
+        CheckBox1.Enabled = True
+        CheckBox2.Enabled = True
+        CheckBox3.Enabled = True
+        Label6.Text = "Inactive"
+        Return 0
+    End Function
+    Function FollowLikes(Browser As OpenQA.Selenium.Chrome.ChromeDriver)
+        AddHandler MyBase.FormClosing, Sub(sender As Object, e As FormClosingEventArgs)
+                                           Browser.Quit()
+                                           Application.Exit()
+                                           Me.Close()
+                                       End Sub
+        Button1.Enabled = False
+        Browser.Navigate.GoToUrl(TextBox5.Text)
+        Dim Following = "#react-root > section > main > div > div.ltEKP > article > div.eo2As > section.EDfFK.ygqzn > div > div > a"
+        Dim TotalFollowing = "#react-root > section > main > div > div.ltEKP > article > div.eo2As > section.EDfFK.ygqzn > div > div > a > span"
+        Dim UnfollowNum As Integer = Int(Browser.FindElementByCssSelector(TotalFollowing).Text)
+        NL("Amount to follow: " + Browser.FindElementByCssSelector(TotalFollowing).Text)
+        Browser.FindElementByCssSelector(Following).Click()
+        Thread.Sleep(1000)
+        NL("Starting to follow " + Browser.FindElementByCssSelector(TotalFollowing).Text + " users")
+        For p As Integer = 1 To UnfollowNum
+            Try
+                Dim rd As New Random()
+                Dim RanNum As Integer = rd.Next(1000, 5000)
+                Thread.Sleep(RanNum)
+                Try
+                    Dim par = Browser.FindElement(By.XPath("/html/body/div[5]/div/div/div[2]/div/div/div[" + p.ToString() + "]/div[3]"))
+
+                    Dim lis = par.FindElements(By.XPath(".//*"))
+                    For Each i In lis
+                        Browser.ExecuteScript("arguments[0].scrollIntoView();", i)
+                        i.Click()
+                    Next
+                Catch ex As Exception
+                    Dim par = Browser.FindElement(By.XPath("/html/body/div[6]/div/div/div[2]/div/div/div[" + p.ToString() + "]/div[3]"))
+                    Dim lis = par.FindElements(By.XPath(".//*"))
+                    For Each i In lis
+                        Browser.ExecuteScript("arguments[0].scrollIntoView();", i)
+                        i.Click()
+                    Next
+                End Try
+                NL("Followed " + p.ToString + "/" + UnfollowNum.ToString())
+            Catch ex As Exception
+                NL("Cannot follow anymore... Sleeping for 5 minutes.")
+                For p1 As Integer = 0 To 300000
+                    Thread.Sleep(1000)
+                    Label6.Text = "Sleeping for " + p1.ToString() + "ms"
+                Next
+            End Try
+        Next
+        NL("Finished following " + UnfollowNum.ToString())
+        Button1.Enabled = True
+        Browser.Quit()
+        CheckBox1.Enabled = True
+        CheckBox2.Enabled = True
+        CheckBox3.Enabled = True
+        Label6.Text = "Inactive"
+        Return 0
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.TextLength <= 2 Then
+            MsgBox("Please input valid account details")
+            Return
+        End If
+        If TextBox2.TextLength <= 2 Then
+            MsgBox("Please input valid account details")
+            Return
+        End If
+        If CheckBox1.Checked = True Then
+            MsgBox("Please do not close the opening Chrome browser!")
+            NL("Starting")
+            Dim Thrd = New Thread(Sub() Login("Follow"))
+            Thrd.Start()
+        ElseIf CheckBox2.Checked = True Then
+            MsgBox("Please do not close the opening Chrome browser!")
+            NL("Starting")
+            Dim Thrd = New Thread(Sub() Login("FollowLikes"))
+            Thrd.Start()
+        ElseIf CheckBox3.Checked = True Then
+            MsgBox("Please do not close the opening Chrome browser!")
+            NL("Starting")
+            Dim Thrd = New Thread(Sub() Login("Unfollow"))
+            Thrd.Start()
+        Else
+            MsgBox("Please pick an option.")
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            CheckBox1.Checked = False
+            CheckBox3.Checked = False
+            TextBox5.ReadOnly = False
+            TextBox4.ReadOnly = True
+        Else
+            TextBox5.ReadOnly = True
+            TextBox4.ReadOnly = True
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            CheckBox2.Checked = False
+            CheckBox3.Checked = False
+            TextBox4.ReadOnly = False
+            TextBox5.ReadOnly = True
+        Else
+            TextBox5.ReadOnly = True
+            TextBox4.ReadOnly = True
+        End If
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked = True Then
+            CheckBox2.Checked = False
+            CheckBox1.Checked = False
+            TextBox5.ReadOnly = True
+            TextBox4.ReadOnly = True
+        Else
+            TextBox5.ReadOnly = True
+            TextBox4.ReadOnly = True
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Application.Exit()
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Viewing = True Then
+            TextBox2.UseSystemPasswordChar = False
+            Viewing = False
+        Else
+            TextBox2.UseSystemPasswordChar = True
+            Viewing = True
+        End If
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+End Class
